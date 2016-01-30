52d4409e6c3f02081bbd79764ca954dc6739e5d6
Michael P. Gerlek <mpg@flaxen.com>
2016-01-30 12:16:42 -0500

    logger interface work

--- common.go
+++ common.go
@@ -1,8 +1,6 @@
 package piazza
 
 import (
-	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
@@ -69,69 +67,6 @@ func Delete(url string) (*http.Response, error) {
 
 //---------------------------------------------------------------------------
 
-// LogMessage represents the contents of a messge for the logger service.
-// All fields are required.
-type LogMessage struct {
-	Service  string `json:"service"`
-	Address  string `json:"address"`
-	Time     string `json:"time"`
-	Severity string `json:"severity"`
-	Message  string `json:"message"`
-}
-
-// ToString returns a LogMessage as a formatted string.
-func (mssg *LogMessage) ToString() string {
-	s := fmt.Sprintf("[%s, %s, %s, %s, %s]",
-		mssg.Service, mssg.Address, mssg.Time, mssg.Severity, mssg.Message)
-	return s
-}
-
-// SeverityDebug is for log messages that are only used in development.
-const SeverityDebug = "Debug"
-
-// SeverityInfo is for log messages that are only informative, no action needed.
-const SeverityInfo = "Info"
-
-// SeverityWarning is for log messages that indicate possible problems. Execution continues normally.
-const SeverityWarning = "Warning"
-
-// SeverityError is for log messages that indicate something went wrong. The problem is usually handled and execution continues.
-const SeverityError = "Error"
-
-// SeverityFatal is for log messages that indicate an internal error and the system is likely now unstable. These should never happen.
-const SeverityFatal = "Fatal"
-
-// Validate checks to make sure a LogMessage is properly filled out. If not, a non-nil error is returned.
-func (mssg *LogMessage) Validate() error {
-	if mssg.Service == "" {
-		return errors.New("required field 'service' not set")
-	}
-	if mssg.Address == "" {
-		return errors.New("required field 'address' not set")
-	}
-	if mssg.Time == "" {
-		return errors.New("required field 'time' not set")
-	}
-	if mssg.Severity == "" {
-		return errors.New("required field 'severity' not set")
-	}
-	if mssg.Message == "" {
-		return errors.New("required field 'message' not set")
-	}
-
-	ok := false
-	for _, code := range [...]string{SeverityDebug, SeverityInfo, SeverityWarning, SeverityError, SeverityFatal} {
-		if mssg.Severity == code {
-			ok = true
-			break
-		}
-	}
-	if !ok {
-		return errors.New("invalid 'severity' setting")
-	}
-
-	return nil
-}
 
 
 //---------------------------------------------------------------------------
