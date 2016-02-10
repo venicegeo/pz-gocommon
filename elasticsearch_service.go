8cbf47bd860c00d8a5423765076d7fbd8d0837bd
Michael P. Gerlek <mpg@flaxen.com>
2016-02-10 16:24:52 -0500

    elastic work

--- elasticsearch_service.go
+++ elasticsearch_service.go
@@ -1,7 +1,10 @@
 package piazza
 
 import (
-	"gopkg.in/olivere/elastic.v3"
+	"gopkg.in/olivere/elastic.v2"
+	"log"
+	"errors"
+	"os"
 )
 
 type ElasticSearchService struct {
@@ -15,12 +18,28 @@ func newElasticSearchService() (es *ElasticSearchService, err error) {
 	client, err := elastic.NewClient(
 		elastic.SetURL("https://search-venice-es-pjebjkdaueu2gukocyccj4r5m4.us-east-1.es.amazonaws.com"),
 		elastic.SetSniff(false),
-		elastic.SetMaxRetries(5),
-		/*elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),*/
-		/*elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags))*/)
+		//elastic.SetMaxRetries(5),
+		elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
+		elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)))
 	if err != nil {
 		return nil, err
 	}
+
+	esversion, err := client.ElasticsearchVersion("https://search-venice-es-pjebjkdaueu2gukocyccj4r5m4.us-east-1.es.amazonaws.com") //("http://127.0.0.1:9200")
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+	log.Printf("Elasticsearch version %s", esversion)
+
+/*	info, code, err := client.Ping().Do()
+	if err != nil {
+		// Handle error
+		log.Printf("panic")
+		panic(err)
+	}
+	log.Printf("Elasticsearch returned with code %d and version %s", code, info.Version.Number)
+*/
 
 	return &ElasticSearchService{Client: client, name: PzElasticSearch, address: "...es.amazonaws.com"}, nil
 }
@@ -41,15 +60,22 @@ func (es *ElasticSearchService) MakeIndex(index string) error {
 	}
 
 	if exists {
-		_, err = es.Client.DeleteIndex(index).Do()
+		deleteIndex, err := es.Client.DeleteIndex(index).Do()
 		if err != nil {
 			return err
 		}
+		if !deleteIndex.Acknowledged {
+			panic(errors.New("ES Delete Not acknowledged!"))
+		}
 	}
 
-	_, err = es.Client.CreateIndex(index).Do()
+	createIndex, err := es.Client.CreateIndex(index).Do()
 	if err != nil {
 		return err
+	}
+
+	if !createIndex.Acknowledged {
+		panic(errors.New("ES Create Not acknowledged!"))
 	}
 
 	return nil
