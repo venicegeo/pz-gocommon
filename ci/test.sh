be5a47804ac584422518511cc322d365f5180280
mpg <mpg@flaxen.com>
2016-06-13 11:49:04 -0400

    add coverage generation

--- ci/test.sh
+++ ci/test.sh
@@ -15,7 +15,7 @@ go get github.com/stretchr/testify/assert
 go get gopkg.in/olivere/elastic.v3
 
 go get github.com/venicegeo/pz-gocommon
-go test -v github.com/venicegeo/pz-gocommon
-go test -v github.com/venicegeo/pz-gocommon/elasticsearch
+go test -v -coverprofile=common.cov github.com/venicegeo/pz-gocommon
+go test -v -coverprofile=elastic.cov github.com/venicegeo/pz-gocommon/elasticsearch
 
 ###
