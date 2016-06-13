5a66c925a9b8f934a37d905b20ed687e0c34845f
mpg <mpg@flaxen.com>
2016-06-13 10:11:09 -0400

    adding to Jenkins
    but just for testing - not actually archiving the lib

---
+++ ci/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
+popd > /dev/null
+
+export GOPATH=$root/gogo
+mkdir -p $GOPATH
+
+###
+
+go get github.com/stretchr/testify/suite
+go get github.com/stretchr/testify/assert
+
+go get gopkg.in/olivere/elastic.v3
+
+go get github.com/venicegeo/pz-gocommon
+go test -v github.com/venicegeo/pz-gocommon
+go test -v github.com/venicegeo/pz-gocommon/elasticsearch
+
+###
