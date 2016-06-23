0922e07fe68321c8108306690a9124165a279fe6
John McMahon <jmcmahon@radiantblue.com>
2016-06-23 11:42:29 -0400

    4340 Add go get statement for gin (should fix jenkins build)

--- ci/test.sh
+++ ci/test.sh
@@ -14,6 +14,8 @@ go get github.com/stretchr/testify/assert
 
 go get gopkg.in/olivere/elastic.v3
 
+go get github.com/gin-gonic/gin
+
 go get github.com/venicegeo/pz-gocommon
 go test -v -coverprofile=common.cov github.com/venicegeo/pz-gocommon
 go test -v -coverprofile=elastic.cov github.com/venicegeo/pz-gocommon/elasticsearch
