4950c77d58c238b6c9be1b2c6fb51c9c54a526d1
mpg <mpg@flaxen.com>
2016-07-25 09:24:14 -0400

    debugging

--- gocommon/http_utils.go
+++ gocommon/http_utils.go
@@ -221,7 +221,7 @@ func GinReturnJson(c *gin.Context, resp *JsonResponse) {
 	if err != nil {
 		log.Fatalf("Internal Error: marshalling of %#v", resp)
 	}
-	//log.Printf("%s", string(raw))
+	log.Printf("%d %s", len(raw), string(raw))
 	c.Data(resp.StatusCode, ContentTypeJSON, raw)
 }
 
