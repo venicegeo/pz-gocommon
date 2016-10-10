a9a2b30bbe983a01efd0a238c14ef918548d6ce3
mpg <mpg@flaxen.com>
2016-10-10 17:05:46 -0400

    linting

--- ci/metalinter.sh
+++ ci/metalinter.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 gometalinter \
---deadline=45s \
---concurrency=8 \
+--deadline=60s \
+--concurrency=6 \
 --vendor \
+--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
 ./...
 
-#--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
 #--exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
 #--exclude="cyclomatic complexity 13 of function createQueryDslAsString" \
 #--exclude="cyclomatic complexity 12 of function \(\*Service\)\.GetMessage" \
