c7c0c8ff74cd6498dc47a668a2eaab71165e4ea9
mpg <mpg@flaxen.com>
2016-11-24 17:22:59 -0500

    added IP function, for use by syslog

--- gocommon/http_utils.go
+++ gocommon/http_utils.go
@@ -21,6 +21,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"strings"
@@ -135,4 +136,43 @@ func GetApiServer() (string, error) {
 		return "", fmt.Errorf("$PZSERVER not set")
 	}
 	return pzserver, nil
+}
+
+// GetExternalIP returns the "best"(?) IP address we can reasonably get.
+// see: http://stackoverflow.com/a/23558495
+func GetExternalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
 }
