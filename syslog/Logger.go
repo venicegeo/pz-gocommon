02ed6f665b1b087fa87b67c8f84b5567b7b34aae
mpg <mpg@flaxen.com>
2017-01-16 09:58:12 -0500

    logs from client to logger are now async

--- syslog/Logger.go
+++ syslog/Logger.go
@@ -51,7 +51,7 @@ func NewLogger(logWriter Writer, auditWriter Writer, application string) *Logger
 		application:      application,
 		hostname:         hostname,
 		processId:        processId,
-		Async:            false,
+		Async:            true,
 	}
 
 	return logger
